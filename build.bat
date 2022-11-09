7f63dad337c61beae96ae759c655e545675cc56f
Utsawb <utsawblamichhane@gmail.com>
2022-11-09 03:54:08 -0600

    changed build file to make a gui app instead of a console one

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 g++ -c main.cpp -O4 -ffast-math -IF:/Code/C++/SFML-2.5.1/include -IF:/Code/C++/attractors/ -DSFML_STATIC
 
-g++ main.o -o main -LF:/Code/C++/SFML-2.5.1/lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lsfml-main
+g++ main.o -o main -LF:/Code/C++/SFML-2.5.1/lib -mwindows -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lsfml-main
 
 del main.o
 
